a3bd25c079e0e22ae8cf9c80de24f17043b8389e
Elivan Kook <elivanmetal@gmail.com>
2018-03-12 18:35:05 +0100

    making the game responsive

--- src/components/FlyingObject.jsx
+++ src/components/FlyingObject.jsx
@@ -3,6 +3,18 @@ import PropTypes from 'prop-types';
 import FlyingObjectBase from './FlyingObjectBase';
 import FlyingObjectTop from './FlyingObjectTop';
 
-const FlyingObject = props => {
-    
-}
+const FlyingObject = props => (
+  <g>
+    <FlyingObjectBase position={props.position} />
+    <FlyingObjectTop position={props.position} />
+  </g>
+);
+
+FlyingObject.propTypes = {
+  position: PropTypes.shape({
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired
+  }).isRequired,
+};
+
+export default FlyingObject;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 //Very wide to provide a full screen feeling
 export const skyAndGroundWidth = 5000;
+
+export const gameWidth = 800;
